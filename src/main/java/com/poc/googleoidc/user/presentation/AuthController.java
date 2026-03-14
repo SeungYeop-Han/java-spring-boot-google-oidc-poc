6bd13003aa67438491c31672dbcd3703f1a75447
SeungYeop-Han <ammezkhan@gmail.com>
2026-03-14 14:40:23 +0900

    auth controller 위치 이동

--- src/main/java/com/poc/googleoidc/user/presentation/AuthController.java
+++ src/main/java/com/poc/googleoidc/user/presentation/AuthController.java
@@ -1,4 +1,4 @@
-package com.poc.googleoidc;
+package com.poc.googleoidc.user.presentation;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
