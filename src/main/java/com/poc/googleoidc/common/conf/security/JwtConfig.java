305b77d684b56d1237e7d65e16d4dcc10226fb7c
SeungYeop-Han <ammezkhan@gmail.com>
2026-03-14 23:57:53 +0900

    스프링 시큐리티 Jwt 빈 팩토리 정의

---
+++ src/main/java/com/poc/googleoidc/common/conf/security/JwtConfig.java
@@ -0,0 +1,37 @@
+package com.poc.googleoidc.common.conf.security;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class JwtConfig {
+
+    @Bean
+    SecretKey jwtSecretKey(
+            @Value("${app.auth.jwt.secret}") String secret
+    ) {
+        return new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(SecretKey jwtSecretKey) {
+        return NimbusJwtDecoder
+                .withSecretKey(jwtSecretKey)
+                .build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(SecretKey jwtSecretKey) {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(jwtSecretKey));
+    }
+}
